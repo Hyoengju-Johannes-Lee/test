e8394626967d5288bebb01a5ae8543c3c9ca0ecd
Hyeongju Johannes Lee <>
2020-04-04 16:34:02 +0300

    User Management Service with ID/Password initial verison

---
+++ api/members/member.service.js
@@ -0,0 +1,51 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+
+const fs = require('fs');
+const data = fs.readFileSync('./database.json');
+const mysql = require('mysql');
+const conf = JSON.parse(data);
+
+const connection = mysql.createConnection({
+   host: conf.host,
+   user: conf.user,
+   password: conf.password,
+   port: conf.port,
+   database: conf.database
+})
+
+connection.connect();
+
+module.exports = {
+    registerMember,
+    deleteMember,
+    getAll
+};
+
+
+async function deleteMember(res, {id, typedPW}) {
+    let sql = 'DELETE FROM MEMBER WHERE id=? and password=AES_ENCRYPT(?, HEX(SHA2(\'encrypt-key\',512)))';
+    let params = [id, typedPW]
+    await connection.query(sql, params,
+        (err, rows, fields) => {
+            if(rows.affectedRows){
+                res.json({success: true})
+            }
+            else{
+                res.json({success: false})
+            }
+        })
+}
+
+async function registerMember(res, {name, id, password, zion, unit}) {
+    let sql = 'INSERT INTO MEMBER VALUES (?, ?, AES_ENCRYPT(?, HEX(SHA2(\'encrypt-key\',512))), ?, ?)';
+    let params = [name, id, password, zion, unit];
+    return await connection.query(sql, params,
+        (err, rows, fields) => { res.send(rows) })
+}
+
+async function getAll(res) {
+    await connection.query(
+     "SELECT name, id, zion, unit FROM MEMBER",
+       (err, rows, fields) => { res.send(rows); });
+}
---
+++ api/members/members.controller.js
@@ -0,0 +1,40 @@
+﻿const express = require('express');
+const router = express.Router();
+const memberService = require('./member.service');
+
+const fs = require('fs');
+const data = fs.readFileSync('./database.json');
+const mysql = require('mysql');
+const conf = JSON.parse(data);
+
+const connection = mysql.createConnection({
+   host: conf.host,
+   user: conf.user,
+   password: conf.password,
+   port: conf.port,
+   database: conf.database
+})
+
+connection.connect();
+
+
+// routes
+router.post('/delete', deleteMember);
+router.post('/register', registerMember);
+router.get('/', getAll);
+
+module.exports = router;
+
+async function registerMember(req, res) {
+    await memberService.registerMember(res, req.body)
+
+}
+
+async function deleteMember(req, res) {
+    await memberService.deleteMember(res, req.body)
+}
+
+
+async function getAll(req, res) {
+    await memberService.getAll(res)
+}
